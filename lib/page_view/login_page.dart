15a801fbf394dddb26bf52917430f74a93acb175
杜子豪 <duzh@wdcloud.cc>
2019-04-30 16:00:03 +0800

    提交

--- lib/page_view/login_page.dart
+++ lib/page_view/login_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
     return Container(
 
       alignment: Alignment.center,
